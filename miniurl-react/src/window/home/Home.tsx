eea2a3c0b2bd4d41461c356baf7fb374a8d293a6
jahidem <jahid.hossain.e99@gmail.com>
2023-04-20 11:01:51 +0600

    making TextArea component for the floating text input UI to get the user URL

--- miniurl-react/src/window/home/Home.tsx
+++ miniurl-react/src/window/home/Home.tsx
@@ -1,10 +1,18 @@
 import NavSection from './components/NavSection';
 import LandingSection from './components/LandingSection';
+import { Box } from '@chakra-ui/react';
+import TextArea from './components/TextArea';
 const Home = () => {
   return (
     <>
       <NavSection/>
       <LandingSection/>
+      <Box position='absolute' mt='-35px'>
+        <TextArea/>
+      </Box>
+      <Box bgColor='#F3F3F4' h='400px'>
+
+      </Box>
     </>
   );
 };
