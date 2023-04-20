7c5fe6489d23022a4e280390548cf7b6807cb8ba
jahidem <jahid.hossain.e99@gmail.com>
2023-04-20 12:13:08 +0600

    FooterSection done

--- miniurl-react/src/window/home/Home.tsx
+++ miniurl-react/src/window/home/Home.tsx
@@ -1,17 +1,21 @@
+import { Box } from '@chakra-ui/react';
 import NavSection from './components/NavSection';
 import LandingSection from './components/LandingSection';
-import { Box } from '@chakra-ui/react';
 import TextArea from './components/TextArea';
 import FeaturesSection from './components/FeaturesSection';
+import FooterSection from './components/FooterSection';
 const Home = () => {
   return (
     <>
-      <NavSection/>
-      <LandingSection/>
-      <Box position='absolute' mt='-35px'>
-        <TextArea/>
+      <NavSection />
+      <LandingSection />
+      <Box
+        position='absolute'
+        mt='-35px'>
+        <TextArea />
       </Box>
-      <FeaturesSection/>
+      <FeaturesSection />
+      <FooterSection/>
     </>
   );
 };
