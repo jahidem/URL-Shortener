b240fc47f68f7ba063beca10f2e9aa6e71bf1a5f
jahidem <jahid.hossain.e99@gmail.com>
2023-04-22 21:08:08 +0600

    refractored StatOfMiniurl component out of Stat component

--- miniurl-react/src/window/stats/Stat.tsx
+++ miniurl-react/src/window/stats/Stat.tsx
@@ -4,6 +4,7 @@ import TextWithCopyButton from '../home/components/TextWithCopyButton';
 import { useState } from 'react';
 import { UrlPair } from '../../utilities/types';
 import { API_SERVER_URL } from '../../utilities/values';
+import StatOfMiniurl from './StatOfMiniurl';
 const Stat = () => {
   const [urlPair, serUrlPair] = useState<UrlPair>({
     id: '0',
@@ -15,55 +16,7 @@ const Stat = () => {
     <>
       <NavSection />
       <hr />
-      <Box
-        maxW='1333px'
-        mx='auto'
-        mt='2.6rem'>
-        <Flex
-          flexDir='column'
-          w='100%'
-          h='100%'
-          justifyContent='space-between'>
-          <TextWithCopyButton
-            textToShow={urlPair.mini_url}
-            textTitle={API_SERVER_URL + '/'}
-            titleWidth='250px'
-          />
-          {urlPair.mini_url && (
-            <>
-              <TextWithCopyButton
-                textToShow={urlPair.original_url}
-                textTitle='Your URL'
-              />
-              <Flex
-                alignSelf='center'
-                mt='5.6rem'
-                alignItems='center'>
-                <Text
-                  fontSize='2.4rem'
-                  p='1.6rem'>
-                  This Tinyurl has been visited
-                </Text>
-                <Box
-                  bgColor='rgba(0, 105, 254,.25)'
-                  borderRadius='9px'>
-                  <Text
-                    color='gray.600'
-                    fontSize='2.4rem'
-                    p='0.4rem'>
-                    {urlPair.visit_count}
-                  </Text>
-                </Box>
-                <Text
-                  fontSize='2.4rem'
-                  p='1.6rem'>
-                  times!
-                </Text>
-              </Flex>
-            </>
-          )}
-        </Flex>
-      </Box>
+      <StatOfMiniurl miniUrlId=''/>
     </>
   );
 };
